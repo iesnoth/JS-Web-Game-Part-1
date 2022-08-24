f23896e6f9a76b314f28a99b2cfacfa21ea158d4
Victoria Beasley <iesnoth91@gmail.com>
2022-08-23 21:33:32 -0500

    done i guess

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(url, left, bottom){
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    return object
+}
+//function for items
+function newItem(url,left,bottom){
+ let object = newImage(url,left,bottom)
+object.addEventListener('dblclick', function(){
+    object.remove()
+})
+}
+
+
+//main character
+newImage('assets/green-character.gif',100,100)
+//pine tree to the right
+newImage('assets/pine-tree.png',450,200)
+//round tree to the left of the pine
+newImage('assets/tree.png',200,300)
+//pillar to right
+newImage('assets/pillar.png',350,100)
+//tiny crate
+newImage('assets/crate.png',150,200)
+//well
+newImage('assets/well.png',500,425)
+
+
+//clickable sword
+newItem('assets/sword.png',500,405)
+//shield
+newItem('assets/sheild.png',165,185)
+//staff
+newItem('assets/staff.png',600,100)
+
+//background
+// function backGround(url,start,finish)
+// {
+
+// }
+
+console.log(window.innerWidth)
